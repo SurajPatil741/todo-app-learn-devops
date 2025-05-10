65e62da6897e4da2b1fc78bae2212ec2a7e9666d
Ubuntu <ubuntu@ip-172-31-10-244.eu-north-1.compute.internal>
2025-05-10 10:19:01 +0000

    Fix flake8 errors

--- todo/urls.py
+++ todo/urls.py
@@ -3,7 +3,13 @@ from . import views  # Import your views
 
 urlpatterns = [
     path(
-        "", views.todo_list, name="todo_list"
-    ),  # This is the path for the main todo list view
-    path("add/", views.add_todo, name="add_todo"),  # Path for adding a new todo item
+        "",
+        views.todo_list,
+        name="todo_list"
+    ),
+    path(
+        "add/",
+        views.add_todo,
+        name="add_todo"
+    ),
 ]
