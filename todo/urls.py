aa9add53af3be0fa40ec00d01d36a1136c202a2e
Ubuntu <ubuntu@ip-172-31-10-244.eu-north-1.compute.internal>
2025-05-11 09:24:56 +0000

    Fix: Auto-format files using Black

--- todo/urls.py
+++ todo/urls.py
@@ -2,14 +2,6 @@ from django.urls import path
 from . import views  # Import your views
 
 urlpatterns = [
-    path(
-        "",
-        views.todo_list,
-        name="todo_list"
-    ),
-    path(
-        "add/",
-        views.add_todo,
-        name="add_todo"
-    ),
+    path("", views.todo_list, name="todo_list"),
+    path("add/", views.add_todo, name="add_todo"),
 ]
